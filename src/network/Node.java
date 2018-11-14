4bb0615849b2ee78c7139306def867c5a692d0eb
Ichimei Sakurai <29670891+ichimei@users.noreply.github.com>
2018-11-14 18:26:08 +0800

    almost done part 2

--- src/network/Node.java
+++ src/network/Node.java
@@ -3,6 +3,9 @@ package network;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Arrays;
 
 import javax.sound.sampled.AudioFormat;
@@ -22,7 +25,6 @@ public class Node {
 	final byte node_tx = (byte) 0xff;
 	final byte node_rx = (byte) 0xff;
 
-	int mac_event_no;
 	final int frames = 10;
 	final int frame_size = 200;
 	final int amp = 32767;        // amplitude
@@ -40,15 +42,17 @@ public class Node {
 	int num_frames = 0;
 	int retry_cnt = 0;
 
-	boolean tx_pending = true;
+	boolean get_ack = true;
 	byte[] received_frame;
 
 	boolean[] header = new boolean[header_size];
 	byte[] intv = new byte[200];
 	byte[][] frame_list;
 	final int header_len = header_size * spb;
+	final int packet_len = (frame_size + 32) * spb;
 	SourceDataLine speak;
 	TargetDataLine mic;
+	FileOutputStream fos;
 
 	public Node() {
 	}
@@ -66,11 +70,13 @@ public class Node {
 		frames_init(file_tx);
 		device_init();
 		mac();
+		new Thread(() -> {
+		    packet_detect();
+		}).start();
 		device_stop();
 	}
 
-	private boolean[] bytes_to_bits(byte[] bytesBuffer) {
-		boolean[] bitsBuffer = new boolean[bytesBuffer.length * 8];
+	private void bytes_to_bits(byte[] bytesBuffer, boolean[] bitsBuffer) {
 		for (int i = 0; i < bytesBuffer.length; ++i) {
 			byte b = bytesBuffer[i];
 			for (int j = 0; j < 8; ++j) {
@@ -78,7 +84,18 @@ public class Node {
 				b >>= 1;
 			}
 		}
-		return bitsBuffer;
+	}
+
+	private void bits_to_bytes(boolean[] bitsBuffer, byte[] bytesBuffer) {
+		for (int i = 0; i < bytesBuffer.length; ++i) {
+			byte b = 0;
+			for (int j = 0; j < 8; ++j) {
+				if (bitsBuffer[i*8+j]) {
+					b |= 1 << j;
+				}
+			}
+			bytesBuffer[i] = b;
+		}
 	}
 
 	private int[] bits_to_ints(boolean[] bits) {
@@ -103,12 +120,21 @@ public class Node {
 
 	private boolean[] get_mac_header(byte dest, byte src, boolean ack) {
 		byte[] mac_header = {dest, src, (byte) (ack ? 0xff : 0)};
-		return bytes_to_bits(mac_header);
+		boolean[] mac_header_bits = new boolean[mac_header.length * 8];
+		bytes_to_bits(mac_header, mac_header_bits);
+		return mac_header_bits;
 	}
 
-	private boolean[] get_crc(byte[] data) {
+	private byte get_crc_byte(byte[] data, int offset, int len) {
 		CRC8 crc8 = new CRC8(CRC_POLYNOM, CRC_INITIAL);
-		crc8.update(data,0,data.length);
+		crc8.update(data, offset, len);
+		byte crc = (byte) crc8.getValue();
+		return crc;
+	}
+
+	private boolean[] get_crc(byte[] data, int offset, int len) {
+		CRC8 crc8 = new CRC8(CRC_POLYNOM, CRC_INITIAL);
+		crc8.update(data, offset, len);
 		byte crc = (byte) crc8.getValue();
 		boolean[] result = new boolean[8];
 		for (int i = 0; i < 8; ++i) {
@@ -131,15 +157,15 @@ public class Node {
 			Arrays.fill(header, true);
 
 			byte[] bytesBuffer = new byte[frame_size / 8];
-			boolean[] bitsBuffer;
+			boolean[] bitsBuffer = new boolean[frame_size];
 			frame_list = new byte[num_frames][];
 			for (int i = 0; i < num_frames; ++i) {
 				ByteArrayOutputStream bos = new ByteArrayOutputStream();
 				fis.read(bytesBuffer);
-				bitsBuffer = bytes_to_bits(bytesBuffer);
+				bytes_to_bits(bytesBuffer, bitsBuffer);
 				writeBits(bos, get_mac_header(node_tx, node_id, false));
 				writeBits(bos, bitsBuffer);
-				writeBits(bos, get_crc(bytesBuffer));
+				writeBits(bos, get_crc(bytesBuffer, 0, bytesBuffer.length));
 				frame_list[i] = bos.toByteArray();
 			}
 			fis.close();
@@ -149,6 +175,13 @@ public class Node {
 	}
 
 	private void device_init() {
+		if (file_rx != null) {
+			try {
+				fos = new FileOutputStream(file_rx);
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
 		AudioFormat format = getAudioFormat();
 		DataLine.Info infoSpeak = new DataLine.Info(SourceDataLine.class, format);
 		DataLine.Info infoMic = new DataLine.Info(TargetDataLine.class, format);
@@ -169,20 +202,24 @@ public class Node {
 		speak.close();
 		mic.stop();
 		mic.close();
+		if (file_rx != null) {
+			try {
+				fos.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
-	private int[] bytes_to_ints(byte[] data) {
-		int[] ints = new int[data.length/2];
+	private void bytes_to_ints(byte[] data, int[] ints) {
 		for (int i = 0; i < data.length / 2; ++i)
 			ints[i] = (data[2*i+1] << 8) | (data[2*i] & 0xff);
-		return ints;
 	}
 
 	private void packet_detect() {
 		byte buffer[] = new byte[header_len * 2];
 		int oldBuffer[] = new int[header_len];
 		int newBuffer[];
-		byte packetBuffer[] = new byte[2000];
 		int header_ints[] = bits_to_ints(header);
 
 		int state = 0;
@@ -194,7 +231,8 @@ public class Node {
 		while (true) {
 			if (state == 0) {
 				mic.read(buffer, 0, header_len*2);
-				newBuffer = bytes_to_ints(buffer);
+				newBuffer = new int[header_len];
+				bytes_to_ints(buffer, newBuffer);
 				for (int i = 1; i <= header_len; ++i) {
 					float syncPower = 0.f;
 					for (int j = 0; j < header_len; ++j) {
@@ -215,9 +253,63 @@ public class Node {
 					oldBuffer = newBuffer;
 				}
 			} else {
-
+				int packetRest_len = packet_len - header_len + start;
+				byte packetRestBuffer[] = new byte[packetRest_len * 2];
+				int[] packetRest = new int[packetRest_len];
+				int[] packet = new int[packet_len];
+				mic.read(packetRestBuffer, 0, packetRest_len * 2);
+				bytes_to_ints(packetRestBuffer, packetRest);
+				System.arraycopy(oldBuffer, start, packet, 0, header_len - start);
+				System.arraycopy(packetRest, 0, packet, header_len - start, packetRest_len);
+				packet_decode(packet);
 			}
 			++while_cnt;
+		}
+	}
+
+	private byte bit8_to_byte(boolean[] bit8, int offset) {
+		byte ret = 0;
+		for (int i = 0; i < 8; ++i) {
+			if (bit8[i]) {
+				ret |= 1 << i;
+			}
+		}
+		return ret;
+	}
+
+	private void packet_decode(int[] packet) {
+		System.out.println("Packet detected!!!");
+		boolean decoded[] = new boolean[packet_len / spb];
+		for (int i = 0; i < decoded.length; ++i) {
+			float sumRmCarr = 0.f;
+			for (int k = 0; k < spb; ++k) {
+				sumRmCarr += (packet[i*spb+k]) * (float) Math.sin(2 * Math.PI * fc * k / fs);
+			}
+			decoded[i] = sumRmCarr > 0.f;
+		}
+		if (bit8_to_byte(decoded, 0) != node_id)
+			return;
+		byte packet_src = bit8_to_byte(decoded, 8);
+		byte type = bit8_to_byte(decoded, 16);
+		if (type > 0x7f) {
+			// ACK!
+			get_ack = true;
+			return;
+		} else {
+			// normal packet!
+			byte decoded_bytes[] = new byte[decoded.length / 8];
+			bits_to_bytes(decoded, decoded_bytes);
+			if (get_crc_byte(decoded_bytes, 3, frame_size / 8) != decoded_bytes[frame_size / 8 + 3]) {
+				// crc failed! pretend not detected
+				return;
+			} else if (file_rx != null) {
+				// this is my packet! write it
+				try {
+					fos.write(decoded_bytes, 3, frame_size / 8);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
 		}
 	}
 
